6121ef0ce92af511a98fe224162813f44f1c2264
Viachaslau <pmstss@gmail.com>
2025-02-20 21:20:05 +0400

    test: generated Bright SecTester e2e security tests

---
+++ test/security/get-partners-query.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { SecRunner } from '@sectester/runner';
+import { TestType, HttpMethod, Severity } from '@sectester/scan';
+
+jest.setTimeout(15 * 60 * 1000); // 15 minutes
+
+let runner!: SecRunner;
+
+beforeEach(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!
+  });
+
+  await runner.init();
+});
+
+afterEach(() => runner.clear());
+
+it('GET /api/partners/query - XPATH Injection', async () => {
+  await runner
+    .createScan({
+      name: expect.getState().currentTestName,
+      tests: [TestType.XPATH_INJECTION],
+      attackParamLocations: []
+    })
+    .threshold(Severity.LOW)
+    .timeout(15 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${process.env.BRIGHT_TARGET_URL}/api/partners/query`,
+      headers: { 'content-type': 'text/xml' },
+      query: { xpath: '/partners/partner/name' }
+    });
+});
