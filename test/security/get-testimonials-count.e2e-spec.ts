17d470e496f1ac25536e6ac938324bdf2faaa01b
Viachaslau <pmstss@gmail.com>
2025-02-25 12:46:25 +0400

    test: generated Bright SecTester e2e security tests

---
+++ test/security/get-testimonials-count.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { SecRunner } from '@sectester/runner';
+import { Severity, TestType, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+jest.setTimeout(25 * 60 * 1000); // 25 minutes
+
+let runner!: SecRunner;
+
+beforeEach(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!
+  });
+
+  await runner.init();
+});
+
+afterEach(() => runner.clear());
+
+it('GET /api/testimonials/count', async () => {
+  await runner
+    .createScan({
+      name: expect.getState().currentTestName,
+      tests: [TestType.SQL_INJECTION, TestType.EXCESSIVE_DATA_EXPOSURE, TestType.EXPOSED_DATABASE_DETAILS],
+      attackParamLocations: [AttackParamLocation.QUERY]
+    })
+    .threshold(Severity.LOW)
+    .timeout(25 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${process.env.BRIGHT_TARGET_URL}/api/testimonials/count`,
+      query: { query: 'select count(*) as count from testimonial' },
+      headers: { 'content-type': 'text/html' }
+    });
+});
