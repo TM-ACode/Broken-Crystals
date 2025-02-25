2348e80dff3a7a5cf3be3adb4ca6c3760e73e1d0
Viachaslau <pmstss@gmail.com>
2025-02-25 20:22:43 +0400

    test: generated Bright SecTester e2e security tests

---
+++ test/security/get-partners-query.e2e-spec.ts
@@ -0,0 +1,53 @@
+import { SecRunner } from '@sectester/runner';
+import { TestType, Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+
+let app!: INestApplication;
+let baseUrl!: string;
+let runner!: SecRunner;
+
+beforeAll(async () => {
+  const moduleFixture = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  app = moduleFixture.createNestApplication({
+    logger: false,
+  });
+  await app.listen(0, '0.0.0.0');
+
+  baseUrl = await app.getUrl();
+
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!
+  });
+
+  await runner.init();
+});
+
+afterAll(async () => {
+  await runner.clear();
+  await app.close();
+});
+
+jest.setTimeout(40 * 60 * 1000);
+
+it('GET /api/partners/query', async () => {
+  await runner
+    .createScan({
+      name: expect.getState().currentTestName,
+      tests: [TestType.XPATH_INJECTION],
+      attackParamLocations: [AttackParamLocation.QUERY],
+      skipStaticParams: false
+    })
+    .threshold(Severity.LOW)
+    .timeout(40 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/partners/query`,
+      headers: { 'content-type': 'text/xml' },
+      query: { xpath: '/partners/partner/name' }
+    });
+});
