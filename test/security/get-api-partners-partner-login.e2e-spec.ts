f31bd2e094424b426d432fd39cbb00256826bbc6
Viachaslau <pmstss@gmail.com>
2025-03-04 14:24:09 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ test/security/get-api-partners-partner-login.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { SecRunner } from '@sectester/runner';
+import { Severity, TestType, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+jest.setTimeout(40 * 60 * 1000); // 40 minutes
+
+let runner!: SecRunner;
+
+beforeEach(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+afterEach(() => runner.clear());
+
+it('GET /api/partners/partnerLogin', async () => {
+  await runner
+    .createScan({
+      tests: [
+        TestType.XPATH_INJECTION,
+        TestType.EXCESSIVE_DATA_EXPOSURE,
+        TestType.INSECURE_OUTPUT_HANDLING
+      ],
+      attackParamLocations: [
+        AttackParamLocation.QUERY,
+        AttackParamLocation.HEADER
+      ]
+    })
+    .threshold(Severity.LOW)
+    .timeout(40 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${process.env.BRIGHT_TARGET_URL}/api/partners/partnerLogin`,
+      headers: {
+        'content-type': 'text/xml'
+      },
+      query: {
+        username: 'walter100',
+        password: 'Heisenberg123'
+      }
+    });
+});
