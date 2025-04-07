2cd3ffa24197e9f25f09cc932197b47111371b15
Bar Hofesh <bar.hofesh@gmail.com>
2025-04-07 15:31:09 +0300

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-auth-admin-login.test.ts
@@ -0,0 +1,38 @@
+import { test, before, after } from 'node:test';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { SecRunner } from '@sectester/runner';
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+test('POST /api/auth/admin/login', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'excessive_data_exposure', 'jwt', 'sqli', 'xss'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/auth/admin/login`,
+      body: {
+        user: 'john',
+        password: 'Pa55w0rd',
+        op: 'basic'
+      }
+    });
+});
