c5bf2b990fedda74c3b5d8d44f4ed2aa7e26f853
Viachaslau <slava@tssbox.com>
2025-06-09 14:29:02 +0400

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-file.test.ts
+++ .brightsec/tests/get-api-file.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/file', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['lfi', 'rfi', 'open_cloud_storage', 'file_upload', 'full_path_disclosure'],
+      tests: ['lfi', 'rfi', 'full_path_disclosure'],
       attackParamLocations: [AttackParamLocation.QUERY]
     })
     .threshold(Severity.CRITICAL)
