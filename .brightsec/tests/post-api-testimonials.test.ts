f85720c3895db6f46dc654dd425812b2a0c14a98
Viachaslau <slava@tssbox.com>
2025-09-15 15:38:20 +0400

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/post-api-testimonials.test.ts
+++ .brightsec/tests/post-api-testimonials.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('POST /api/testimonials', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['xss', 'sqli', 'csrf', 'jwt', 'bopla'],
+      tests: ['jwt'],
       attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.HEADER],
       starMetadata: { databases: ['PostgreSQL'] }
     })
